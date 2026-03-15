31340bf36d59de0bf09fe28ac9e85415a09e3979
Tanmay Sahu <tanmaysahu03092001@gmail.com>
2026-03-14 23:55:12 -0400

    fixed: government dashboard crash

--- src/views/GovDashboard.jsx
+++ src/views/GovDashboard.jsx
@@ -7,6 +7,7 @@ import { clusterResources, computeBarrierIndex } from '../utils/mlScoring'
 import FilterBar from '../components/FilterBar'
 import MapView from '../components/MapView'
 import ExportButton from '../components/ExportButton'
+import TravelBurdenPanel from '../components/TravelBurdenPanel'
 
 const CLUSTER_COLORS = ['#22c55e', '#3b82f6', '#f59e0b', '#ef4444']
 const CLUSTER_LABELS_KEY = ['wellServed', 'moderateAccess', 'strained', 'foodDesert']
