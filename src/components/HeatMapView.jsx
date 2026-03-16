b9b51704b14b68279ef5cc51f7536c0951e3cedc
Tanmay Sahu <tanmaysahu03092001@gmail.com>
2026-03-15 22:34:28 -0400

    Fix misplaced MarkerClusterGroup imports causing runtime crash

--- src/components/HeatMapView.jsx
+++ src/components/HeatMapView.jsx
@@ -1,5 +1,6 @@
 import { MapContainer, TileLayer, Circle, Marker, Tooltip, useMap } from 'react-leaflet'
 import L from 'leaflet'
+import MarkerClusterGroup from 'react-leaflet-cluster'
 import { useEffect, useMemo, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { getRiskLabel } from '../utils/mlScoring'
@@ -366,8 +367,6 @@ export default function HeatMapView({
         />
 
         <FlyToCenter center={center} zoom={zoom} shouldFly={false} />
-
-import MarkerClusterGroup from 'react-leaflet-cluster'
 
         <SnapLayer rows={rows} mode={mode} />
 
--- src/components/HeatMapViewNYNJ.jsx
+++ src/components/HeatMapViewNYNJ.jsx
@@ -1,5 +1,6 @@
 import { MapContainer, TileLayer, Circle, CircleMarker, Marker, Tooltip, useMap } from 'react-leaflet'
 import L from 'leaflet'
+import MarkerClusterGroup from 'react-leaflet-cluster'
 import { useEffect, useMemo, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { getRiskLabel } from '../utils/mlScoring'
@@ -369,8 +370,6 @@ export default function HeatMapViewNYNJ({
         />
 
         <FlyToCenter center={center} zoom={zoom} shouldFly={false} />
-
-import MarkerClusterGroup from 'react-leaflet-cluster'
 
         <SnapLayer rows={rows} mode={mode} />
 
